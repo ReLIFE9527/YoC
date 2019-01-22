84f52870317eafae3701b3503254e96f1537c004
FrierenZk <lingfengfroze@yahoo.com>
2019-01-22 20:23:13 +0800

    realize envpath

--- Common/envpath.go
+++ Common/envpath.go
@@ -1,13 +1,40 @@
 package envpath
 
-import "runtime"
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
 
-var os = runtime.GOOS
+var osType = runtime.GOOS
 
-func GetPath(path string){
-	
+func GetPath(path string) string {
+	var ret string
+	var err error
+	switch osType {
+	case "windows":
+		path = strings.Replace(path, "/", "\\", -1)
+		ret, err = filepath.Abs("./")
+		if err != nil {
+			log.Fatal("Can't get path")
+		}
+		if path[0] != '\\' {
+			path = "\\" + path
+		}
+		ret += path
+	case "linux":
+		if path[0] == '/' {
+			path = path[1:]
+		}
+		ret = "/root/" + path
+	default:
+	}
+	fmt.Println("Log path now is" + ret)
+	return ret
 }
 
-func GetLogPath(){
-
+func GetLogPath(project string)string {
+	return GetPath("logs/" + project + ".log")
 }
