2a9ef433791bcfcbdbde7ec40111b768a6d574dc
FrierenZk <lingfengfroze@yahoo.com>
2019-01-29 19:06:06 +0800

    Build devices i/o framework

--- Device/handleconnection.go
+++ Device/handleconnection.go
@@ -4,27 +4,61 @@ import (
 	. "../Log"
 	. "../Manager"
 	"fmt"
+	"io"
 	"net"
+	"sync"
+	"time"
 )
 
+var writeOp = make(map[string][]string)
+var writeMutex = make(map[string]*sync.Mutex)
+
 func handleConnection(conn net.Conn) (err error) {
-	var addr = conn.RemoteAddr().String()
+	var addr, beatBreak, beatFresh = conn.RemoteAddr().String(), false, make(chan string)
 	IMDeviceLogin(addr)
-	Log.Println(addr," : connected")
+	writeOp[addr] = make([]string, 0)
+	writeMutex[addr] = new(sync.Mutex)
+	Log.Println(addr, " : connected")
+	go connectionHeartBeats(&beatBreak, beatFresh)
 	//TODO
 	for {
-		var buffer= make([]byte, 128)
-		n, err := conn.Read(buffer)
-		if err == nil && n > 0 {
-			data := string(buffer[:n])
-			fmt.Println(data)
+		if len(writeOp[addr]) > 0 {
+			writeMutex[addr].Lock()
+			op := writeOp[addr][0]
+			writeOp[addr] = writeOp[addr][1:]
+			writeMutex[addr].Unlock()
+			err = dispatchOp(op, conn)
+			beatFresh <- ""
+		} else {
+			var buffer = make([]byte, 128)
+			err = conn.SetReadDeadline(time.Now().Add(time.Microsecond * 50))
+			if err != nil {
+				Log.Println(err)
+			}
+			n, err := conn.Read(buffer)
+			if err == nil && n > 0 {
+				data := string(buffer[:n])
+				err = dispatchRead(data, conn)
+				beatFresh <- ""
+			} else {
+				if n == -1 {
+					//break
+					fmt.Println(err)
+				}
+				switch err {
+				case io.EOF:
+					beatBreak = true
+				}
+			}
 		}
-		if false {
+		if beatBreak {
 			break
 		}
 	}
 	defer func() {
 		IMDeviceLogout(addr)
+		writeMutex[addr] = nil
+		writeOp[addr] = nil
 		err = conn.Close()
 		if err != nil {
 			Log.Println(err)
@@ -32,5 +66,25 @@ func handleConnection(conn net.Conn) (err error) {
 			Log.Println("connection closed :", addr)
 		}
 	}()
+	return err
+}
+
+func connectionHeartBeats(flag *bool,ch chan string){
+	for {
+		select {
+		case <-ch:
+		case <-time.After(time.Minute):
+			*flag = true
+			break
+		}
+	}
+}
+
+func dispatchOp(str string,conn net.Conn)  (err error) {
+	return err
+}
+
+func dispatchRead(str string,conn net.Conn)(err error) {
+	fmt.Println(str)
 	return err
 }
