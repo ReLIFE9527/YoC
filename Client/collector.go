b946dfc404b87ccb9896c90d29d1480a637c02e3
FrierenZk <lingfengfroze@yahoo.com>
2019-02-12 20:03:57 +0800

    Create collector.go

---
+++ Client/collector.go
@@ -0,0 +1,102 @@
+package Client
+
+import (
+	. "../Log"
+	"../Pack"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"time"
+)
+
+type Collector struct {
+	*Connector
+	id        string
+	key       string
+	operation string
+	working   chan string
+}
+
+func (collector *Collector) loop() {
+
+}
+
+func (collector *Collector) extraInit() {
+	collector.working = make(chan string, 1)
+}
+
+func (collector *Collector) preAction() {
+
+}
+
+func (collector *Collector) postAction() {
+
+}
+
+func (collector *Collector) checkAccess() error {
+	const loginFailed, loginDone Pack.Stream = "{\"login\":\"failed\"}", "{\"login\":\"done\"}"
+	var access = make(chan string, 1)
+	go collector.verify(access)
+	select {
+	case key := <-access:
+		if key != "" {
+			stream := Pack.Convert2Stream(&map[string]string{"key": key})
+			packet := Pack.StreamPack(stream)
+			err := collector.writeRepeat(packet, time.Second)
+			if err != nil {
+				return io.EOF
+			}
+			err = collector.writeRepeat(Pack.StreamPack(loginDone), time.Second)
+			return err
+		} else {
+			if key == collector.key {
+				err := collector.writeRepeat(Pack.StreamPack(loginDone), time.Second)
+				return err
+			} else {
+				_ = collector.writeRepeat(Pack.StreamPack(loginFailed), time.Second)
+				return io.EOF
+			}
+		}
+	case <-time.After(time.Second * 10):
+		go func() {
+			access <- ""
+			time.Sleep(time.Second)
+			<-access
+		}()
+		return io.EOF
+	}
+}
+
+func (collector *Collector) verify(ch chan string) {
+	var bytes string
+	for len(ch) == 0 {
+		_ = collector.conn.SetReadDeadline(time.Now().Add(time.Millisecond * 100))
+		bytes, _ = collector.readWriter.ReadString(Pack.TailByte)
+		packet := Pack.Packet(bytes)
+		if len(packet) > 0 {
+			stream, err := Pack.DePack(packet)
+			if err != nil {
+				Log.Println(err)
+			} else {
+				if Pack.IsStreamValid(stream, []string{"id"}) {
+					var table = Pack.Convert2Map(stream)
+					if id, ok := (*table)["id"]; ok {
+						//get key from IM
+						id += ""
+						if key, ok := (*table)["key"]; ok {
+							ch <- key
+						} else {
+							key = fmt.Sprintf("%x", sha1.Sum([]byte(time.Now().String())))
+							ch <- key
+							//override the key
+							collector.key = key
+						}
+					} else {
+						ch <- ""
+					}
+				}
+			}
+		}
+	}
+	_ = collector.conn.SetReadDeadline(time.Time{})
+}
