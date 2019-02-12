384481716a6c8822ee5e036945267bede8bf7fc2
FrierenZk <lingfengfroze@yahoo.com>
2019-02-12 22:37:51 +0800

    Remove getmap

--- Data/repository.go
+++ Data/repository.go
@@ -1,25 +1,11 @@
 package Data
 
 import (
-	"reflect"
 	"time"
 )
 
 type repository struct {
-	id        string
-	key       string
-	lastLogin time.Time
-	this      *map[string]interface{}
-}
-
-func (stock *repository) GetMap() map[string]interface{} {
-	var key = reflect.TypeOf(stock)
-	var value = reflect.ValueOf(stock)
-	stock.this = new(map[string]interface{})
-	for i := 0; i < key.NumField(); i++ {
-		if key.Field(i).Name != "this" {
-			(*stock.this)[key.Field(i).Name] = value.Field(i)
-		}
-	}
-	return *stock.this
+	ID        string
+	Key       string
+	LastLogin time.Time
 }
