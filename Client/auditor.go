eed1d6cf0721ce85e21b69e59d245f79679de21e
FrierenZk <lingfengfroze@yahoo.com>
2019-02-13 11:22:33 +0800

    Add auditor function

--- Client/auditor.go
+++ Client/auditor.go
@@ -1,7 +1,46 @@
 package Client
 
-import "net"
+import (
+	. "../Log"
+	"net"
+)
 
-type auditor struct {
-	listener net.Listener
+type Auditor struct {
+	listener         net.Listener
+	network, address string
+}
+
+func (auditor *Auditor) Init() error {
+	auditor.network = "udp"
+	auditor.address = "localhost:12345"
+	err := auditor.subInit()
+	auditor.listener, err = net.Listen(auditor.network, auditor.address)
+	if err == nil {
+		Log.Println(auditor.network, auditor.address, "Waiting for connection...")
+	}
+	return err
+}
+
+func (auditor *Auditor) subInit() error {
+	return nil
+}
+
+func (auditor *Auditor) Listen(errCh chan error) {
+	for len(errCh) == 0 {
+		conn, err := auditor.listener.Accept()
+		if err != nil {
+			errCh <- err
+		} else {
+			go auditor.handle(conn)
+		}
+	}
+	defer func() {
+		err := auditor.listener.Close()
+		Log.Println(err)
+		Log.Println(auditor.network, auditor.address, "listener closed.")
+	}()
+}
+
+func (auditor *Auditor) handle(conn net.Conn) {
+	return
 }
